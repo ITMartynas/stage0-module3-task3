b72af653dad923adf2caef4ce62b1660f8298893
Martynas <martynas.bartnykas@gmail.com>
2023-12-09 15:42:17 +0200

    Conducted tasks related to assigning and changing variables values.

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -2,6 +2,21 @@ package school.mjc.stage0.module3.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        int first = 1;
+        int second = 10;
+        int third = 100;
+
+        System.out.println(first + "\n" + second + "\n" + third);
+
+        int linkToFirst = 15;
+        int linkToSecond = 6;
+        int linkToThird = 4;
+
+        first = 1;
+        second = 10;
+        third = 100;
+
+
+        System.out.println(linkToFirst + "\n" + linkToSecond + "\n" + linkToThird + "\n" + first + "\n" + second + "\n" + third);
     }
 }
